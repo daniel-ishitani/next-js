1899970a155abf33ebedbb9bcf396537b26b5536
daniel-ishitani <ishitanidim@gmail.com>
2022-06-28 19:44:40 -0300

    static site generation

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,12 +1,34 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-function Post() {
-  const router = useRouter();
+import data from '../../data.json';
+
+export async function getStaticPaths() {
+  const paths = data.posts.map(post => ({
+    params: { slug: post.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false // false or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const post = data.posts.find(post => post.slug === context.params.slug);
+
+  return {
+    props: post,
+  }
+}
+
+function Post(props) {
+  // const router = useRouter();
 
   return (
     <div>
-      <p>Post!</p>
+      <p>Post {props.slug}</p>
+      <p>{props.content}</p>
 
       <Link href="/">
         <a>Home</a>
